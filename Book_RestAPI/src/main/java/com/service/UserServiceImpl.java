9113625284003fc200c12308a79547262db533be
Ishita Maurya <86301182+ishitam77@users.noreply.github.com>
2023-11-24 13:14:04 +0530

    Update UserServiceImpl.java

--- Book_RestAPI/src/main/java/com/service/UserServiceImpl.java
+++ Book_RestAPI/src/main/java/com/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.service;
 
-import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,12 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dao.UserDao;
 import com.entity.BookEntity;
 import com.entity.UserEntity;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 
 @Service
 @Transactional
@@ -55,51 +50,10 @@ public class UserServiceImpl implements UserService {
             existingUser.setName(updatedUser.getName());
             existingUser.setEmailId(updatedUser.getEmailId());
             existingUser.setOccupation(updatedUser.getOccupation());
-
-            // Create a map of existing books for efficient lookup
-            Map<Integer, BookEntity> existingBooksMap = existingUser.getBooks().stream()
-                    .collect(Collectors.toMap(BookEntity::getBookId, Function.identity()));
-
-            // Update and add books from the updated user
-            for (BookEntity updatedBook : updatedUser.getBooks()) {
-                if (updatedBook.getBookId() == 0) {
-                    // Assign a new bookId if it's not specified (assuming 0 means not specified)
-                    updatedBook.setBookId(getNextBookId(existingUser.getBooks()));
-                }
-
-                // If the book exists in the existing user's books, update it
-                if (existingBooksMap.containsKey(updatedBook.getBookId())) {
-                    BookEntity existingBook = existingBooksMap.get(updatedBook.getBookId());
-                    existingBook.setIsbn(updatedBook.getIsbn());
-                    existingBook.setTitle(updatedBook.getTitle());
-                    existingBook.setType(updatedBook.getType());
-                    existingBook.setPrice(updatedBook.getPrice());
-                    existingBook.setAuthor(updatedBook.getAuthor());
-                } else {
-                    // If the book doesn't exist, add it to the user's books
-                    updatedBook.setUser(existingUser);
-                    existingUser.getBooks().add(updatedBook);
-                }
-            }
-
-            // Remove books that are not in the updated list
-            existingUser.getBooks().removeIf(book -> !updatedUser.getBooks().contains(book));
-
             userDao.save(existingUser);
-
-            // Add logging statements
-            System.out.println("Updated User: " + existingUser);
-            System.out.println("Updated Books: " + existingUser.getBooks());
         }
     }
-    private int getNextBookId(List<BookEntity> books) {
-        return books.stream()
-                .mapToInt(BookEntity::getBookId)
-                .max()
-                .orElse(0) + 1;
-    }
-
-    
+  
     @Override
     public void updateBook(BookEntity updatedBook) {
         bookService.updateBook(updatedBook);
@@ -125,24 +79,17 @@ public class UserServiceImpl implements UserService {
                     existingBook.setType(updatedBook.getType());
                     existingBook.setPrice(updatedBook.getPrice());
                     existingBook.setAuthor(updatedBook.getAuthor());
-
-                    // Save the user within the same transaction
                     userDao.save(existingUser);
-
-                    // Add logging statements
                     System.out.println("Updated User: " + existingUser);
                     System.out.println("Updated Book: " + existingBook);
                 } else {
-                    // Handle the case where the book is not found
                     System.out.println("Book not found for user with ID: " + userId);
                 }
             } else {
-                // Handle the case where the user is not found
                 System.out.println("User not found with ID: " + userId);
             }
         } catch (Exception e) {
             e.printStackTrace();
-            // Handle the exception, e.g., log or throw a custom exception
         }
     }
 
